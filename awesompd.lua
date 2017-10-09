61065ca9c55573dbec7a7ce60db87668a2350ed2
MaksVal <valmaxster@gmail.com>
2017-10-09 09:10:02 +0300

    fix a problem with a timer in the smart_updte function

--- awesompd.lua
+++ awesompd.lua
@@ -1061,7 +1061,7 @@ function awesompd:smart_update()
       local pos = to_seconds(self.track_position)
       local dur = to_seconds(self.track_duration)
       local rem = (dur - pos) + 1
-      if (rem < self.update_interval) then
+      if (rem < self.update_interval and rem > 0) then
 	 -- Little time remaining, lets update when it runs out 
 	 local smart_timer = timer({ timeout = rem })
 	 smart_timer:connect_signal("timeout", function()
